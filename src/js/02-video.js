6934bb52e1288aaac28099541fec875575017af8
campot05 <daniil.spectr@gmail.com>
2023-01-17 18:14:59 +0200

    add npm throttle

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playTime = throttle(evn => {
+  localStorage.setItem('videoplayer-current-time', evn.seconds);
+  console.log(localStorage.getItem('videoplayer-current-time'));
+}, 1000);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', playTime);
