a0a19af87815b1bcde3ceb028728bf2cb6cbe800
campot05 <daniil.spectr@gmail.com>
2023-01-17 19:38:16 +0200

    task2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,15 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
+const keyValue = localStorage.getItem(CURRENT_TIME);
 
 const playTime = throttle(evn => {
   localStorage.setItem(CURRENT_TIME, evn.seconds);
-  console.log(localStorage.getItem(CURRENT_TIME));
 }, 1000);
 
-player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+if (keyValue) {
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+  console.log(localStorage.getItem(CURRENT_TIME));
+}
+
 player.on('timeupdate', playTime);
