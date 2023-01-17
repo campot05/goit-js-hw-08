bba8d202169eb7606dd93bd12e4670bf277e1cb2
campot05 <daniil.spectr@gmail.com>
2023-01-17 19:12:39 +0200

    add favicon

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,6 +4,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const CURRENT_TIME = 'videoplayer-current-time';
+
 const playTime = throttle(evn => {
   localStorage.setItem(CURRENT_TIME, evn.seconds);
   console.log(localStorage.getItem(CURRENT_TIME));
